3faecd7d7708e008a572b55a585821aea25e1bcf
renan <renan.mmerlo@gmail.com>
2024-02-07 17:42:50 -0300

    Pipe Operator

---
+++ scripts/basics/pipe_operator/pipe_operator.exs
@@ -0,0 +1,47 @@
+#Pipe Operator
+#Passa o resultado da funcao como primeiro parametro para a proxima
+#vantagem: ligibilidade
+
+defmodule SemPipe do
+  def soma(n1,n2) do
+    n1 + n2
+  end
+
+  def subtracao(n1,n2) do
+    n1 - n2
+  end
+
+  def multiplicacao(n1,n2) do
+    n1 * n2
+  end
+
+  def divisao(n1,n2) do
+    n1 / n2
+  end
+
+  def tudo(n1,n2) do
+    divisao(multiplicacao(subtracao(soma(n1,n2), n2), n2),n2)
+  end
+end
+
+defmodule ComPipe do
+  def soma(n1,n2) do
+    n1 + n2
+  end
+
+  def subtracao(n1,n2) do
+    n1 - n2
+  end
+
+  def multiplicacao(n1,n2) do
+    n1 * n2
+  end
+
+  def divisao(n1,n2) do
+    n1 / n2
+  end
+
+  def tudo(n1,n2) do
+    soma(n1,n2) |> subtracao(n2) |> multiplicacao(n2) |> divisao(n2)
+  end
+end
